5e761323330ec32fa728dd676a579e1b781ecd15
Developer <developer@example.com>
2026-07-26 23:45:18 +0800

    Disable foreign key constraints during database seed import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         $sqlPath = base_path('database_export.sql');
         if (File::exists($sqlPath)) {
+            Schema::disableForeignKeyConstraints();
             DB::unprepared(File::get($sqlPath));
+            Schema::enableForeignKeyConstraints();
         }
     }
 }
